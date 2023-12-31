7ebfc2c0c66e91c1f41a4275c063628c8ae3a5b7
umair <umairalism@gmail.com>
2024-01-01 00:58:15 +0500

    need to revise it again

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,9 +1,10 @@
-from cnnClassifier.constants import *
 import os
-from cnnClassifier.utils.common import read_yaml, create_directories,save_json
+from cnnClassifier.constants import *
+from cnnClassifier.utils.common import read_yaml, create_directories
 from cnnClassifier.entity.config_entity import (DataIngestionConfig,
                                                 PrepareBaseModelConfig,
-                                                TrainingConfig)
+                                                TrainingConfig,
+                                                EvaluationConfig)
 
 
 class ConfigurationManager:
@@ -34,8 +35,6 @@ class ConfigurationManager:
         return data_ingestion_config
     
 
-
-    
     def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
         config = self.config.prepare_base_model
         
@@ -56,12 +55,11 @@ class ConfigurationManager:
     
 
 
-
     def get_training_config(self) -> TrainingConfig:
         training = self.config.training
         prepare_base_model = self.config.prepare_base_model
         params = self.params
-        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "kidney-ct-scan-image")
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "Chest-CT-Scan-data")
         create_directories([
             Path(training.root_dir)
         ])
@@ -79,3 +77,16 @@ class ConfigurationManager:
 
         return training_config
     
+
+
+
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/artifacts/data_ingestion/kidney-ct-scan-image",
+            mlflow_uri="https://dagshub.com/m-umairali/Kidney-Disease-Classification-Deep-Learning-Project.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
